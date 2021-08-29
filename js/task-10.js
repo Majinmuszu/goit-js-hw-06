c282d24b6ea441f3abb3f8e9332dccdb8bc330b6
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-08-29 17:08:42 +0200

    damn it

--- js/task-10.js
+++ js/task-10.js
@@ -1,38 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
 const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
 const log = (something) => console.log(something);
 
-const numOfBoxes = qs('input')
-const addBoxes = qs('button[data-create]')
-const removeBoxes = qs('button[data-destroy]')
-const divOfBoxes = qs('#boxes')
-log(addBoxes)
-log(removeBoxes)
-log(numOfBoxes)
+const input = qs('input');
+const create = qs('button[data-create]');
+const destroy = qs('button[data-destroy]');
+const boxes = qs('#boxes');
+const divBoxes = qsa("#boxes div");
+const value = input.value;
 
-const createBoxes = (amount) => {
-  let value = numOfBoxes.value;
-  let size = 30;
+const createBoxes = () => {
+  let size = 20;
+  for (let i = 0; i < value; i++) {
+    size += 10;
+    let randomColor = getRandomHexColor;
+    
+    const box = document.createElement('div');   
 
-  for (let i = 1; i <= value; i++) {
-    const box = document.createElement('div');
-    divOfBoxes.append(box);
-    let divBox = qs('#boxes>div');
-    log(divBox)
-    divBox.style.backgroundColor = 'red' ;
-    divBox.style.width = '30px';
-    divBox.style.height = '30px';
+    box.style.backgroundColor = `${randomColor}`;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.margin = "3px";
+    box.style.borderRadius = "2px";
+
+    boxes.append(box);
   };
-  
-  
-  log(boxes);
-  log(amount)
 };
+
 const destroyBoxes = () => {
-  let divBox = qs('#boxes>div');
-  divBox.remove();
-};
-addBoxes.addEventListener('click', createBoxes);
-removeBoxes.addEventListener('click', destroyBoxes)
+}; 
+
+create.addEventListener('click', createBoxes);
+destroy.addEventListener('click', destroyBoxes);
